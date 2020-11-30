8b2214657c5052560a470c69e24bea2f10eec9d1
Santiago Vivas <sago.vivas@gmail.com>
2020-11-30 12:44:03 -0500

    Creación de proyecto con Spring

---
+++ src/test/java/co/edu/unal/reto5/Reto5ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unal.reto5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
