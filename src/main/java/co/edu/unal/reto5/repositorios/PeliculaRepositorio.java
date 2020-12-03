2327971df2b8dbcd87a10c15e5932bf9e11d3da0
Javier Gallo <javier@javiergalloroca.com>
2020-12-02 22:54:02 -0500

    Crear repositorio de pelicula

---
+++ src/main/java/co/edu/unal/reto5/repositorios/PeliculaRepositorio.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unal.reto5.repositorios;
+
+import co.edu.unal.reto5.modelos.Pelicula;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author happygallo
+ */
+
+@Repository
+public interface PeliculaRepositorio extends JpaRepository<Pelicula, Long>{
+    
+}
